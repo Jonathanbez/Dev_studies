8a7ce70dca53322ccbeca81c51d4d0408a88258f
Jonathanbez <jbbarroz@gmail.com>
2023-10-06 11:33:16 -0300

    2nd studies of conditionals with ruby and python

--- ruby_studies/condicional.rb
+++ ruby_studies/condicional.rb
@@ -4,14 +4,14 @@
 # IF (verifica se a condiçao é verdadeira (true)
 # se sim executa as intruçoes dentro do corpo da estrutura)
 
-print "Code using conditional IF"
+print "Code using conditional IF\n"
 day = 'friday'
 if day == 'friday'
     coffee_break = 'special'
 end
 
-puts "Lunch is #{coffee_break} today"
-print "End of code using IF"
+puts "Lunch is #{coffee_break} today!"
+print "End of code using IF\n"
 
 ######################################################
 
@@ -23,6 +23,3 @@ print "End of code using IF"
 
 
 # UNLESS (contrario ao if, é executado apenas quando a condiçao é falsa)
-
-
-# CASE ()
