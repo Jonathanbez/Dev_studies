75931e9037008ae30a796fc8a9c2e73dd0c9cfea
Jonathanbez <jbbarroz@gmail.com>
2023-10-09 12:57:52 -0300

    6nd studies of conditionals with ruby

--- ruby_studies/condicional.rb
+++ ruby_studies/condicional.rb
@@ -45,4 +45,15 @@ idade = gets.chomp.int_i
     puts "Voce é uma criança."
  end
 
+ ####################################################
+
 # UNLESS (contrario ao if, é executado apenas quando a condiçao é falsa)
+# IF NOT é parecido com UNLESS
+product_status = 'closed'
+
+unless product_status == 'closed'
+   check_change = 'can'
+else
+   check_change = 'can not'
+end
+puts "You #{check_change} change the product"
