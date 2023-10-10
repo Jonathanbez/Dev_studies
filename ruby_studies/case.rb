fe11fcbf6167d4e43756a9ab9408f094ba150f7a
Jonathanbez <jbbarroz@gmail.com>
2023-10-10 07:16:18 -0300

    13nd studies of conditionals with ruby

--- ruby_studies/case.rb
+++ ruby_studies/case.rb
@@ -1,44 +1,44 @@
-#CASE
 
 require 'date'
 
 print "Quer saber qual seu signo?\n"
-
 print "Digite sua data de nascimento (DD/MM/YYYY): \n"
 
 date_of_birth = gets.chomp
 
-date_of_birth = Date.parse(date_of_birth)
+begin
+  date_of_birth = Date.strptime(date_of_birth, "%d/%m/%Y")
 
-date_of_birth = date_of_birth.strftime('%d/%m')
+  signo = case date_of_birth
+          when Date.new(date_of_birth.year,3,21)..Date.new(date_of_birth.year,4,19)
+            'Áries'
+          when Date.new(date_of_birth.year,4,20)..Date.new(date_of_birth.year,5,20)
+            'Touro'
+          when Date.new(date_of_birth.year,5,21)..Date.new(date_of_birth.year,6,20)
+            'Gêmeos'
+          when Date.new(date_of_birth.year,6,21)..Date.new(date_of_birth.year,7,22)
+            'Câncer'
+          when Date.new(date_of_birth.year,7,23)..Date.new(date_of_birth.year,8,22)
+            'Leão'
+          when Date.new(date_of_birth.year,8,23)..Date.new(date_of_birth.year,9,22)
+            'Virgem'
+          when Date.new(date_of_birth.year,9,23)..Date.new(date_of_birth.year,10,22)
+            'Libra'
+          when Date.new(date_of_birth.year,10,23)..Date.new(date_of_birth.year,11,21)
+            'Escorpião'
+          when Date.new(date_of_birth.year,11,22)..Date.new(date_of_birth.year,12,21)
+            'Sagitário'
+          when Date.new(date_of_birth.year,12,22)..Date.new(date_of_birth.year,12,31), Date.new(date_of_birth.year,1,1)..Date.new(date_of_birth.year,1,19)
+            'Capricórnio'
+          when Date.new(date_of_birth.year,1,20)..Date.new(date_of_birth.year,2,18)
+            'Aquário'
+          when Date.new(date_of_birth.year,2,19)..Date.new(date_of_birth.year,3,20)
+            'Peixes'
+          else
+            'Não encontrado'
+          end
 
-signo = "Não encontrado"
-
-case date_of_birth
-when '21/03'..'19/04'
-    signo = 'Áries'
-when '20/04'..'20/05'
-    signo = 'Touro'
-when '21/05'..'20/06'
-    'Gêmeos'
-when '21/06'..'22/07'
-    signo = 'Câncer'
-when '23/07'..'22/08'
-    signo = 'Leão'
-when '23/08'..'22/09'
-    signo = 'Virgem'
-when '23/09'..'22/10'
-    signo = 'Libra'
-when '23/10'..'21/11'
-    signo = 'Escorpião'
-when '22/11'..'21/12'
-    signo = 'Sagitário'
-when '22/12'..'19/01'
-    signo = 'Capricórnio'
-when '20/01'..'18/02'
-    signo = 'Aquário'
-when '19/02'..'20/03'
-    signo = 'Peixes'
+  puts "Seu signo é: #{signo}"
+rescue ArgumentError
+  puts "Data de nascimento inválida. Por favor, insira no formato DD/MM/YYYY."
 end
-
-puts "Seu signo é: #{signo}"
---
+++ ruby_studies/sign.rb
@@ -0,0 +1,43 @@
+require 'date'
+
+print "Quer saber qual seu signo?\n"
+print "Digite sua data de nascimento (DD/MM/YYYY): \n"
+
+begin
+  date_of_birth = gets.chomp
+  date_of_birth = Date.parse(date_of_birth)
+  date_of_birth = date_of_birth.strftime('%d/%m')
+
+  signo = case date_of_birth
+          when '21/03'..'19/04'
+            'Áries'
+          when '20/04'..'20/05'
+            'Touro'
+          when '21/05'..'20/06'
+            'Gêmeos'
+          when '21/06'..'22/07'
+            'Câncer'
+          when '23/07'..'22/08'
+            'Leão'
+          when '23/08'..'22/09'
+            'Virgem'
+          when '23/09'..'22/10'
+            'Libra'
+          when '23/10'..'21/11'
+            'Escorpião'
+          when '22/11'..'21/12'
+            'Sagitário'
+          when '22/12'..'19/01'
+            'Capricórnio'
+          when '20/01'..'18/02'
+            'Aquário'
+          when '19/02'..'20/03'
+            'Peixes'
+          else
+            'Não encontrado'
+          end
+
+  puts "Seu signo é: #{signo}"
+rescue ArgumentError
+  puts "Data de nascimento inválida. Por favor, insira no formato DD/MM/YYYY."
+end
