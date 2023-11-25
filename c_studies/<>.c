2b2e8e7a46e07a449991b877336bc5936374550b
Jonathanbez <jbbarroz@gmail.com>
2023-11-24 23:02:51 -0300

    C Language learning and test

---
+++ c_studies/<>.c
@@ -0,0 +1,19 @@
+
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    
+    printf("Escreva dois numeros, o mair ira ser mostrado");
+    printf("Escreva o primeiro numero: ");
+    scanf("%d", &a);
+    printf("Escreva o segundo numero: ");
+    scanf("%d", &b);
+
+    if (a > b)
+        print(%)
+
+}
